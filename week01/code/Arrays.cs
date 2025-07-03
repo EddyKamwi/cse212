8e9e24afbebd91f3d6d6979527315c16905c86e5
Edward Kamwi <157596090+EddyKamwi@users.noreply.github.com>
2025-07-03 23:54:05 +0200

    implimenting multiplesOf and RotateListRight functions

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,18 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //1st create an array to store multiples called results
+        var results = new double[length];
+
+        //2nd loop to check if the length is not exceeded
+        //and append to the results list each time
+        for (int count = 0; count <= length; count++)
+        {
+            //assigning values to the array
+            results[count] = (count + 1) * number;
+        }
+
+        return results; // replace this return statement with your own
     }
 
     /// <summary>
@@ -23,11 +36,24 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
-    public static void RotateListRight(List<int> data, int amount)
+    public static List<int> RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //declare an index value for slicing the array
+        var index = data.Count - amount;
+
+        //create a new array starting from the index to end
+        List<int> newList = data.GetRange(index, amount);
+
+        //then append to the new array from beggining to where it was sliced
+        newList.AddRange(data.GetRange(0,index));
+
+        //return the new array
+        return newList;
     }
+
 }
